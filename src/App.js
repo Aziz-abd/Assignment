61b6438febe6cd02a75972b047d9360ff553a78f
Abdulaziz Alabd <aziz.it.buss@gmail.com>
2023-08-07 13:41:36 +0400

    forth commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import AboutSection from "./Components/AboutSection";
 import BottomSection from "./Components/BottomSection";
@@ -11,8 +12,70 @@ import StickyCard from "./Components/StickyCard";
 import VenuSection from "./Components/VenuSection";
 
 function App() {
+  const [showBar, setshowBar] = useState(false);
+  const [selected, setselected] = useState(0);
+
+  function setfixed(params) {
+    if (window.scrollY >= 800) {
+      if (showBar !== true) {
+        setshowBar(true);
+      }
+    } else if (window.scrollY < 800) {
+      if (showBar !== false) {
+        setshowBar(false);
+      }
+    }
+  }
+  window.addEventListener("scroll", setfixed);
   return (
     <div>
+      {showBar && (
+        <nav className=" sticky z-50 left-0 right-0 -mx-32 top-0 w-full h-15 bg-white shadow-md active:transition-all ease-in-out delay-700}">
+          <div className=" flex flex-row items-center px-20 pt-6">
+            <button
+              className={
+                selected === 0
+                  ? "pb-4 border-b-2 border-[#222727] text-sm font-semibold"
+                  : "pb-4 border-b-2 border-transparent text-sm font-semibold hover:border-gray-200"
+              }
+              onClick={() => setselected(0)}
+            >
+              Photos
+            </button>
+
+            <button
+              className={
+                selected === 1
+                  ? "pb-4 border-b-2 border-[#222727] ml-8 text-sm font-semibold"
+                  : "pb-4 ml-8 border-b-2 border-transparent text-sm font-semibold hover:border-gray-200"
+              }
+              onClick={() => setselected(1)}
+            >
+              Service
+            </button>
+            <button
+              className={
+                selected === 2
+                  ? "pb-4 border-b-2 border-[#222727] ml-8 text-sm font-semibold"
+                  : "pb-4 ml-8 border-b-2 border-transparent text-sm font-semibold hover:border-gray-200"
+              }
+              onClick={() => setselected(2)}
+            >
+              Reviews
+            </button>
+            <button
+              className={
+                selected === 3
+                  ? "pb-4 border-b-2 border-[#222727] ml-8 text-sm font-semibold"
+                  : "pb-4 ml-8 border-b-2 border-transparent text-sm font-semibold hover:border-gray-200"
+              }
+              onClick={() => setselected(3)}
+            >
+              About
+            </button>
+          </div>
+        </nav>
+      )}
       <div
         style={{
           margin: "1rem 4.8rem 2rem 4.8rem",
@@ -21,20 +84,24 @@ function App() {
         <MYYHeader />
         <RoutesRow />
         <PhotoSection />
-        <div className="flex mt-12">
-          <div className="w-3/5 ">
-            <ServiceSection />
-            <ReviewSection />
-            <AboutSection />
+
+        <div>
+          <div className="flex mt-12">
+            <div className="w-3/5 ">
+              <ServiceSection />
+              <ReviewSection />
+              <AboutSection />
+            </div>
+
+            <StickyCard />
           </div>
-        
-          <StickyCard/>
         </div>
+
         <VenuSection />
         <BottomSection />
         <div className="mt-16"></div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
